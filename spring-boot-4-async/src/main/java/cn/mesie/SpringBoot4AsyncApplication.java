4f7f71d843780f47ef4aa0fe51d8819860307960
littlemesie <littlemesie@163.com>
2018-12-12 23:44:41 +0800

    spring-boot使用异步

---
+++ spring-boot-4-async/src/main/java/cn/mesie/SpringBoot4AsyncApplication.java
@@ -0,0 +1,12 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4AsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot4AsyncApplication.class, args);
+	}
+}
