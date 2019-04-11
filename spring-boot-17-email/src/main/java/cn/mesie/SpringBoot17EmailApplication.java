23d9e2ccc706dd7cbfa57235c758f1892c529415
littlemesie <littlemesie@163.com>
2019-04-12 00:32:46 +0800

    email

---
+++ spring-boot-17-email/src/main/java/cn/mesie/SpringBoot17EmailApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot17EmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot17EmailApplication.class, args);
+    }
+
+}
