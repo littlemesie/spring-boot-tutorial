a43c442734da63c3575caf7e6ff8e2df73c9633a
littlemesie <littlemesie@163.com>
2019-04-12 00:28:04 +0800

    mongodb

---
+++ spring-boot-14-mongodb/src/main/java/cn/mesie/SpringBoot14MongodbApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot14MongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot14MongodbApplication.class, args);
+    }
+
+}
