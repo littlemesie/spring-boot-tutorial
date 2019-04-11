b73c8a6c3f48a057d4cd7d44530e445d9b50c375
littlemesie <littlemesie@163.com>
2019-04-12 00:25:55 +0800

    es

---
+++ spring-boot-13-es/src/main/java/cn/mesie/SpringBoot13EsApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot13EsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot13EsApplication.class, args);
+    }
+
+}
