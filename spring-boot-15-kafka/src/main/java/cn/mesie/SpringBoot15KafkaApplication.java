69a6432c4e1f802e76b921f187609bed27e0ef74
littlemesie <littlemesie@163.com>
2019-04-12 00:29:24 +0800

    kafka

---
+++ spring-boot-15-kafka/src/main/java/cn/mesie/SpringBoot15KafkaApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot15KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot15KafkaApplication.class, args);
+	}
+
+}
