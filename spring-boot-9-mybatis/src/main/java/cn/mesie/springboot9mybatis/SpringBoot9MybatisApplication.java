b44f0959b13c3e0f841bb0a040c606b28533a464
littlemesie <littlemesie@163.com>
2019-04-12 00:17:01 +0800

    mybatis

---
+++ spring-boot-9-mybatis/src/main/java/cn/mesie/springboot9mybatis/SpringBoot9MybatisApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie.springboot9mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot9MybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot9MybatisApplication.class, args);
+	}
+
+}
---
+++ spring-boot-9-mybatis/src/test/java/cn/mesie/springboot9mybatis/SpringBoot9MybatisApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.mesie.springboot9mybatis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot9MybatisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
