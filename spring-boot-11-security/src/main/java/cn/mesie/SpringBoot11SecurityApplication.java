1779db51f559fa7b19bbef7547c7803f4e4749fa
littlemesie <littlemesie@163.com>
2019-04-12 00:20:35 +0800

    security

---
+++ spring-boot-11-security/src/main/java/cn/mesie/SpringBoot11SecurityApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot11SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot11SecurityApplication.class, args);
+    }
+
+}
