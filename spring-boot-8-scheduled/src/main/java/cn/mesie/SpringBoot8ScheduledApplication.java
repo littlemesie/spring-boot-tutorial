10d4a6088999051c0b708290557612bf8ea65ab0
littlemesie <littlemesie@163.com>
2019-04-12 00:12:23 +0800

    scheduled

---
+++ spring-boot-8-scheduled/src/main/java/cn/mesie/SpringBoot8ScheduledApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot8ScheduledApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot8ScheduledApplication.class, args);
+    }
+
+}
