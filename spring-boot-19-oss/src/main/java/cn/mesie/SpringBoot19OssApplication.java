353858bfbfee03a6ca512736aded87282c417133
littlemesie <littlemesie@163.com>
2019-04-12 00:35:20 +0800

    oss

---
+++ spring-boot-19-oss/src/main/java/cn/mesie/SpringBoot19OssApplication.java
@@ -0,0 +1,13 @@
+package cn.mesie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot19OssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot19OssApplication.class, args);
+	}
+
+}
