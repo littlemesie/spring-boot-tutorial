8d9afd4101e2043a88805a577465eb30db4eefcc
littlemesie <littlemesie@163.com>
2019-04-18 23:31:17 +0800

    exception

--- spring-boot-7-exception/src/main/java/cn/mesie/controller/IndexController.java
+++ spring-boot-7-exception/src/main/java/cn/mesie/controller/IndexController.java
@@ -1,9 +1,47 @@
-package main.java.cn.mesie.controller;
+package cn.mesie.controller;
+
+import cn.mesie.exception.MineException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by 2019-04-14 23:22
  *
  * @author: mesie
  */
+@Controller
 public class IndexController {
+
+    private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/")
+    public String hello(ModelMap map) {
+        logger.info("访问到了 hello ...");
+        map.addAttribute("index", "一起尝试 spring boot 全局异常捕获吧...");
+        return "index";
+    }
+
+    @RequestMapping("/anyone")
+    public String anyone(ModelMap map) throws Exception {
+        logger.info("访问到了 index ...");
+        throw new Exception();
+    }
+
+    @RequestMapping("/mine")
+    public String mine(ModelMap map) {
+        logger.info("访问到了\t\tmine ...");
+        Throwable throwable = new Throwable();
+        throw new MineException("我们访问到了 mine ...", throwable);
+    }
+
+    @ResponseBody
+    @RequestMapping("/sum")
+    public String sum(ModelMap map) throws ArithmeticException {
+        int i = 1 / 0;
+        return "" + i;
+    }
 }
--- spring-boot-7-exception/src/main/java/cn/mesie/exception/ErrorMassage.java
+++ spring-boot-7-exception/src/main/java/cn/mesie/exception/ErrorMassage.java
@@ -1,9 +1,57 @@
-package main.java.cn.mesie.exception;
+package cn.mesie.exception;
 
 /**
  * Created by 2019-04-14 23:21
  *
  * @author: mesie
  */
-public class ErrorMassage {
+public class ErrorMassage<T> {
+
+    public static final Integer OK = 0;
+    public static final Integer ERROR = 100;
+
+    private Integer code;
+    private String message;
+    private String url;
+    private T data;
+
+    public static Integer getOK() {
+        return OK;
+    }
+
+    public static Integer getERROR() {
+        return ERROR;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
